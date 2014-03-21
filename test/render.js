375edf551c705a769b2530010772bc4949ec2486
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-03-20 22:28:49 -0400

    Broke out tests into separate files with slight test refactors

--- test/render.js
+++ test/render.js
@@ -1,66 +1,7 @@
 var expect = require("chai").expect;
-var fs = require("fs");
-var path = require("path");
 var Pagemaki = require("../pagemaki");
 
-var raw = "---\n\
-name: Test Name\n\
-layout: default\n\
----\n\
-<h1>Title</h1>";
-
-
-describe("Parsing raw page data", function () {
-
-	var maker = new Pagemaki({});
-
-	it("should parse yaml options by default", function (done) {
-
-		maker.parse(raw, function (err, parsed) {
-
-			expect(err).to.be.null;
-			expect(parsed).to.be.an('object');
-
-			done();
-
-		});
-
-	});
-
-
-	it("should parse options", function (done) {
-
-		maker.parse(raw, function (err, parsed) {
-
-			expect(parsed.options).to.be.an('object');
-			expect(parsed.options.name).to.equal("Test Name");
-			expect(parsed.options.layout).to.equal("default");
-
-			done();
-
-		});
-
-	});
-
-
-	it("should parse content", function (done) {
-
-		maker.parse(raw, function (err, parsed) {
-
-			expect(parsed.content).to.be.a('string');
-			expect(parsed.content).to.equal("<h1>Title</h1>");
-
-			done();
-
-		});
-	
-	});
-
-
-});
-
-
-describe("Templates", function () {
+describe("render", function () {
 
   var testParsed = {
     test: "a test string"
@@ -70,7 +11,8 @@ describe("Templates", function () {
     return "<h1><%= page.test %></h1>"; 
   };
 
-	it("should render using underscore templates by default", function (done) {
+
+	it("should use underscore templates by default", function (done) {
 
     var maker = new Pagemaki({
       getTemplateString: testRenderer
@@ -86,7 +28,9 @@ describe("Templates", function () {
 
 	});
 
-  it("should accept a custom renderer", function (done) {
+
+
+  it("should be customizeable on creation", function (done) {
 
     var maker = new Pagemaki({
       getTemplateString: testRenderer,
